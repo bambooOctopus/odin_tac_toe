97387b04ddb6479f4dfe6dd99013024f1c29498a
bamboo_octopus <bamboooctopus@gmail.com>
2023-01-29 17:58:18 -0500

    redo upto gameController; add legal move to gameBoard

--- scratch.js
+++ scratch.js
@@ -1,4 +1,4 @@
-const personFactory = (name, moniker) => {    
+const Player = (name, moniker) => {    
     const getName = () => {
         return name;
     };    
@@ -11,6 +11,52 @@ const personFactory = (name, moniker) => {
 };
 
 const Board = () => {
-    
+    let boardArray = [];
+
+    const newBoard = () => {
+        for (i=0; i < 9; i++) {
+            boardArray.push("-");
+        };
+    };
+
+    const legalMove = (position) => {
+        if (boardArray[position] === "-") {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+    const addPiece = (position, playerMoniker) => {
+        if (boardArray[position] === "-") {
+            boardArray.splice(position, 1, playerMoniker);
+        }
+        else {
+            return "invalid move";
+        }
+    };
+
+    return {boardArray, newBoard, legalMove, addPiece}
 };
 
+const gameController = () => {
+    const playerOne = Player("Player One", "x");
+    const playerTwo = Player("Player Two", "o");
+    const gameBoard = Board();
+
+    const newGame = () => {
+        //set new gameBoard
+        gameBoard.newBoard();
+    };
+
+    
+
+    return {playerOne, playerTwo, gameBoard, newGame};
+};
+
+let g = gameController();
+g.newGame();
+console.log(g.gameBoard.boardArray);
+g.gameBoard.addPiece(6, "x");
+console.log(g.gameBoard.boardArray);
