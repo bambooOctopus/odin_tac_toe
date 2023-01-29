3d3c1bf154f32f291a928f8e028d79831782aae6
bamboo_octopus <bamboooctopus@gmail.com>
2023-01-29 18:15:01 -0500

    basic playerTurn

--- scratch.js
+++ scratch.js
@@ -43,11 +43,53 @@ const Board = () => {
 const gameController = () => {
     const playerOne = Player("Player One", "x");
     const playerTwo = Player("Player Two", "o");
+    let currentPlayer = playerOne;
     const gameBoard = Board();
+
+    let blocks = document.querySelectorAll(".move");
+    blocks.forEach(block => block.addEventListener("click", (event) => {
+        console.log(currentPlayer.getMoniker())
+            playerTurn(event.target);
+
+    }));
+
+
 
     const newGame = () => {
         //set new gameBoard
         gameBoard.newBoard();
+    };
+
+    //player turn would run on click?
+    const playerTurn = (eventTarget) => {
+        console.log(eventTarget.id.split("-")[1])
+        //take click input; verify it's a legal move
+        let moveId = eventTarget.id.split("-")[1];
+        if (gameBoard.legalMove(moveId)) {            
+            gameBoard.addPiece(moveId, currentPlayer.getMoniker());
+            console.log(gameBoard.boardArray);
+        }
+        else {
+            return;
+        };
+
+        
+
+        //if (currentPlayer === playerOne) {
+            //check to see if it's a legal move
+
+            //and if it is make the move (add it to the boardArray; eventually update dom)
+
+            //then check to see if it is game over
+
+            //if it isn't game over switch players 
+
+            //if it is game over end the game
+        //}
+        //else {
+
+        //};
+
     };
 
     
@@ -57,6 +99,4 @@ const gameController = () => {
 
 let g = gameController();
 g.newGame();
-console.log(g.gameBoard.boardArray);
-g.gameBoard.addPiece(6, "x");
-console.log(g.gameBoard.boardArray);
+
